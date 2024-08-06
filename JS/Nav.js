33ce00c80c9920c1ffb3202e39474e4342ac5c36
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-08-06 16:01:36 -0500

    init

---
+++ JS/Nav.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/Nostaw/Nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('Nav').innerHTML = data;
+        });
+        fetch('/Nostaw/Footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('Footer').innerHTML = data;
+        });
+ document.querySelector('.sidenav-toggle').addEventListener('click', function() {
+            document.querySelector('.sidenav').classList.toggle('open');
+        });       
+    });
+    
---
+++ README.md
@@ -0,0 +1,2 @@
+# Nostaw
+This is my attempt at making a web app for apple that repackages my website. Its called nostaw becasue that is my Lastname backwards and it sounds pretty cool.
