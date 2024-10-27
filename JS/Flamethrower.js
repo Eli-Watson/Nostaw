e6413ff04bb77ea3386093ea9299cc4562c0b227
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-10-27 13:51:20 -0500

    Changeing from web adaptation on normal site to somthing that looks like neal.fun

---
+++ JS/Flamethrower.js
@@ -0,0 +1,2 @@
+import flamethrower from 'https://cdn.jsdelivr.net/npm/flamethrower-router@0.0.0-meme.12/dist/main.min.js';
+const router = flamethrower({ prefetch: 'visible', log: true, pageTransitions: false });
