8c6c490418d6f72b32aa12d0b7c593c16131ae45
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-10-31 13:15:02 -0500

    Update Flamethrower.js

--- Scripts/Flamethrower.js
+++ Scripts/Flamethrower.js
@@ -1,2 +1,6 @@
 import flamethrower from 'https://cdn.jsdelivr.net/npm/flamethrower-router@0.0.0-meme.12/dist/main.min.js';
 const router = flamethrower({ prefetch: 'visible', log: true, pageTransitions: false });
+
+// Nav Controls
+document.querySelector('.sidenav-toggle').addEventListener('click', function() {
+document.querySelector('.sidenav').classList.toggle('open');
